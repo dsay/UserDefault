cddedb0a0c483c100978d83ba00b92d90408005d
Dmitriy <dmitriy.sai2013@gmail.com>
2021-03-09 15:19:44 +0200

    Initial Commit

---
+++ Sources/UserDefault/UserDefault.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T> {
+    let key: String
+    let defaultValue: T
+
+    init(_ key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
+
+@propertyWrapper
+struct NullableUserDefault<T> {
+    let key: String
+
+    init(_ key: String) {
+        self.key = key
+    }
+
+    var wrappedValue: T? {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
